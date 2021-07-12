6ffa597806371c4b29436ccf9f3c340cea99bc88
DiliniNanayakkara <56678028+DiliniNanayakkara@users.noreply.github.com>
2021-07-13 00:58:13 +0530

    Add modarator and View users (Admin part)

--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/Artworks.css';
+import '../css/User.css';
 import AdminNavbar from '../AdminNavbar';
 
 export default function Users (){
@@ -8,26 +8,110 @@ export default function Users (){
       return (
   
 
-        <div className="A"> 
+        <div className="AB"> 
         <AdminNavbar/>
-
-        <center>
-        <h1>De'lart Users Details</h1></center>
-   
-       
-
-        
-          <div className='C'>
-           <img className="Photo" src='https://www.artshedonline.com.au/assets/images/easels.jpg' alt="" width="1350" height="500"></img>
-          
-        
-
-          
-          </div>
-          
-          
+        <center><h1>Modarator Details</h1></center>
+<center>
   
+        <table className="Artist">
+        <tr>
+    <th>Name</th>
+    <th>UserName</th>
+    <th>ModaratorId</th>
+    <th>Contact No</th>
+    <th>Email</th>
+    <th>Joined Date</th>
+  </tr>
+  <tr>
+    <td>Nimaya</td>
+    <td>Nimaya98</td>
+    <td>M001</td>
+    <td>0771232111</td>
+    <td>Nimaya@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  <tr>
+  <td>Thilini</td>
+    <td>Thilini12</td>
+    <td>M002</td>
+    <td>0771232111</td>
+    <td>Thill@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  <tr>
+  <td>Amasha</td>
+    <td>amasha98</td>
+    <td>M003</td>
+    <td>0771232111</td>
+    <td>amasha@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  <tr>
+  <td>Janani</td>
+    <td>Janani13</td>
+    <td>M001</td>
+    <td>0771232111</td>
+    <td>ann@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  
+  
+    
 
+        </table>
+
+        </center>
+        <center><h1>Artist Details</h1></center>
+<center>
+  
+        <table className="Artist">
+        <tr>
+    <th>Name</th>
+    <th>UserName</th>
+    <th>ModaratorId</th>
+    <th>Contact No</th>
+    <th>Email</th>
+    <th>Joined Date</th>
+  </tr>
+  <tr>
+    <td>Nimaya</td>
+    <td>Nimaya98</td>
+    <td>M001</td>
+    <td>0771232111</td>
+    <td>Nimaya@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  <tr>
+  <td>Thilini</td>
+    <td>Thilini12</td>
+    <td>M002</td>
+    <td>0771232111</td>
+    <td>Thill@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  <tr>
+  <td>Amasha</td>
+    <td>amasha98</td>
+    <td>M003</td>
+    <td>0771232111</td>
+    <td>amasha@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  <tr>
+  <td>Janani</td>
+    <td>Janani13</td>
+    <td>M001</td>
+    <td>0771232111</td>
+    <td>ann@gmail.com</td>
+    <td>12/12/2021</td>
+  </tr>
+  
+  
+    
+
+        </table>
+
+        </center>
 </div>
       );
   
