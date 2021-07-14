1a8480363fadd44995be9d7b1bdba54763b644d7
DiliniNanayakkara <56678028+DiliniNanayakkara@users.noreply.github.com>
2021-07-14 21:41:54 +0530

    Home Page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,30 +1,179 @@
 import React from 'react';
 import '../css/Home.css';
 import Navbar from '../Navbar';
+import home  from '../../assests/home.png';
 export default function Home (){
-
+  
+  
   
       return (
-  
+        
 
         <div className="A"> 
         <Navbar />
-        <center>
-        <h1>De'lart HOME</h1></center>
+        
    
        
 
-        
+        <center>
           <div className='C'>
-           <img className="Photo" src='https://afremov.com/images/product/image_653.jpeg' alt="" width="1350" height="500"></img>
-          
-        
+           <center><img className="Photo" src= { require('../../assests/home.png').default} alt="" width="1200" height="334"></img>
+           </center>
+           <center><h1>What De'lart is ?</h1>
+        <p>Do you still suffer from not being able to find the design you want? Grief is over
+De'lart is the best way to buy the best artwork you want.This allows you to see the creations of many artists on a single stage. So you will not miss anyone. Do you want the 
+creations of a novice artist or the creations of a mature artist? Signup now to find your artist.
+</p>
+<p>Are you an artist? Then we will do a great service for you too. Signup as an artist here now. Through this you can sell your creations. It's also easy for you to get the design tools you need here.
+We make sure to bring valuable high-quality stuff</p></center>
 
           
           </div>
+          </center>
+          <div className="pencil">
+            <img className="pencile" src= { require('../../assests/artworks.png').default} alt="" width="300" height="300"></img>
+        
+            </div>
           
+          <div className="XX">
+          <h1 className="text">Find Arts You Love</h1>
+          </div>
+
+          <h2 className="title">Discover Art You Love From the Online Gallery</h2>
+          <p className="Info">Online you can find works that are currently at artotheque but also works that are at artists' workrooms. It is possible to reserve also those by contacting us.
+
+Here you can search works by technique, color, theme, and size.
+
+If you find an artwork that interests you, do contact us and ask more. We are happy to assist you in selecting the right work for you also on spot.</p>
+          <button className="button">Sign Up it's Free</button>
+        
+         <center> <div className="all" >
+           <h1>Tour Our Art Works Gallery</h1>
+          <div className="pencilarts">
+            <div className="Arts">
+            <center><img className="p" src='https://www.cmuse.org/wp-content/uploads/2020/05/learn-oil-painting-lessons-online.jpg' alt="" width="220" height="180"></img></center>
+            <p>1200LkR</p>
+            <div className="buy"><button>Buy</button></div>
+
+            </div>
+            <div className="Arts">
+              <center><img className="p" src='https://m.media-amazon.com/images/I/91ZTBvEQU4L._AC_SL1500_.jpg' alt="" width="220" height="180"></img></center>
+              <p>1200LkR</p>
+              <div className="buy"><button>Buy</button></div>
+              
+            </div>
+            <div className="Arts">
+            <center><img className="p" src='https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/70acf3fc-aaac-4b39-b5c0-adb2978390ff/watercolorpainting-53.jpg' alt="" width="220" height="180"></img></center>
+            <p>1200LkR</p>
+            <div className="buy"><button>Buy</button></div>
+              
+            </div>
+            <div className="Arts">
+            <center><img className="p" src='https://webneel.com/daily/sites/default/files/images/daily/12-2013/1-watercolor-painting-by-vilas%20kulkarn.jpg' alt="" width="220" height="180"></img></center>
+            <p>1200LkR</p>
+            <div className="buy"><button>Buy</button></div>
+            </div>
+          </div>
+          </div></center>
+          <div className="pencil">
+            <img className="pencile" src= { require('../../assests/pencil.png').default} alt="" width="300" height="300"></img>
+        
+            </div>
           
-  
+          <div className="XY">
+          <h1 className="text">Customize your Pencil Art</h1>
+          </div>
+
+          <h2 className="title">Custom Black pencile Portrails</h2>
+          <h3 className="Info">Your photo Become a Black pencil Drawing by professional portrait Artists</h3>
+          <button className="button">Sign Up it's Free</button>
+        
+         <center> <div className="all" >
+           <h1>Tour Our Black Pencile Art Gallery</h1>
+          <div className="pencilart">
+            <div className="Art">
+            <center><img className="p" src='https://www.cuded.com/wp-content/uploads/2013/02/Franco-Clun600_428.jpg' alt="" width="220" height="180"></img></center>
+            
+
+            </div>
+            <div className="Art">
+              <center><img className="p" src='https://i0.wp.com/novocom.top/image/YWUwMljZG4S5hbGljZG4uY29t/kf/HTB1Kynqar1YBuNjSszhq6AUsFXax/Hair-face-sadness-tears-girl-sadness-portrait-Living-room-home-art-decor-wood-frame-fabric-poster.jpg' alt="" width="220" height="180"></img></center>
+              
+            </div>
+            <div className="Art">
+            <center><img className="p" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW2lhs0CQDa-an33qlz1DRXIZ3IGcU0N0TqQ&usqp=CAU' alt="" width="220" height="180"></img></center>
+              
+            </div>
+            <div className="Art">
+            <center><img className="p" src='https://www.pencilsketchportraits.co.uk/wp-content/uploads/2018/10/daughter-father-pencil-portrait-1030x736.jpg' alt="" width="220" height="180"></img></center>
+            </div>
+          </div>
+          </div></center>
+          
+          <div className="XY">
+          <h1 className="text">Buy the Art Tools you need.</h1>
+          </div>
+
+          
+          <p className="Infotools">Buy all the brushes, watercolors you need, painting, pastels, and art papers under one roof.</p>
+          <button className="button">Sign Up for Buy</button>
+        
+         <center> <div className="all" >
+          
+          <div className="pencilart">
+           <center>  <img className="tool" src= { require('../../assests/tool.png').default} alt=""></img></center>
+          </div>
+          </div></center>
+        <div className="footer">
+        <div className="footercard">
+          <h1>De'lart</h1>
+          <h3>Contact Us</h3>
+          <p>Contact Us</p>
+          <p>Legal Notices</p>
+          <p>General Terms and Conditions</p>
+          <p>My Profile</p>
+          <p>Help Center</p>
+          <p>Call: +119876677</p>
+          <p>Emails:delart@gmail.com</p>
+          
+        </div>
+        <div className="footercard">
+        <h3>Who are we</h3>
+          <p>About Us</p>
+          <p>Our Artists</p>
+          <p>Exibitions</p>
+          <p>Contact</p>
+          <p>Our PRoducts</p>
+         
+         
+        
+        </div>
+        <div className="footercard">
+        <h3>Contact Us</h3>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="footercard">
+        <h3>Contact Us</h3>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+          <p>Contact Us</p>
+        </div>
+        </div>
+       
 
 </div>
       );
