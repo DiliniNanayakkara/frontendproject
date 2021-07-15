15d84a0e8a990cae4efae221fb30a6c864d18a20
Amasha Gamage <42163897+amasha98@users.noreply.github.com>
2021-07-15 18:56:11 +0800

    set footer

--- src/components/pages/Artworks.js
+++ src/components/pages/Artworks.js
@@ -98,6 +98,51 @@ export default function Artworks() {
               })}
               
         </div>
+         <div className="footer">
+        <div className="footercard">
+          <h1>De'lart</h1>
+          <h3>Contact Us</h3>
+          <p>Contact Us</p>
+          <p>Legal Notices</p>
+          <p>General Terms and Conditions</p>
+          <p>My Profile</p>
+          <p>Help Center</p>
+          <p>Call: +119876677</p>
+          <p>Emails:delart@gmail.com</p>
+          
+        </div>
+        <div className="footercard">
+        <h3>Who are we</h3>
+          <p>About Us</p>
+          <p>Our Artists</p>
+          <p>Exibitions</p>
+          <p>Contact</p>
+          <p>Our PRoducts</p>
+         
+         
+        
+        </div>
+        <div className="footercard">
+        <h3>Are you an Artist</h3>
+          <p>How to join Delart</p>
+          <p>Login for Artist</p>
+          <p>Art Gallery</p>
+          <h3>Are you an Buyer</h3>
+          <p>How to join Delart</p>
+          <p>Login for Buyer</p>
+          <p>Art Gallery</p>
+          <p>Artists</p>
+         
+         
+        </div>
+        <div className="footercard">
+        <h3>Sign Up</h3>
+          <p>Artist can usr this system for sell their Artworks.Buyers can purches Artworks.
+            First SignUp ! it's Free!
+          </p>
+          <center><div className="fbutton">SignUp</div></center>
+        </div>
+        </div>
     </div>
   );
 }
