30781ac0820b7680f332cece23070f79474c7f31
DiliniNanayakkara <dlnnimesha@gmail.com>
2021-08-20 15:16:11 +0530

    update pencile arts

--- src/components/pages/Customer_Pencilarts.js
+++ src/components/pages/Customer_Pencilarts.js
@@ -44,7 +44,23 @@ export default function Pencilarts() {
                         </tr>
                         <tr>
                               <td>Nimaya</td>
-                              <td><h2>More Info</h2><p>Historical Art Exhibition.Available Arts:2000</p></td>
+                              <td><h2>More Info</h2><p>Hello!
+
+
+
+                                    I am a freelance artist and I do any kind of illustration including digital pencil sketches. I can sketch anything you want me to, from people, to animals, to nature, etc.
+
+
+
+                                    I will deliver work according to the package you choose for this gig. You will get a 3000px x 3000px image in 300 dpi, printable resolution.
+
+
+
+                                    Please message me first for inquiries before placing an order.
+
+
+
+                                    Thanks!</p></td>
                               <td className="B">2000LKR</td>
                               <td><button className="notavailable">Show Artist Artworks</button></td>
 
