84e964b75f67890ac634f7e2fb3a4884b826b741
DiliniNanayakkara <dlnnimesha@gmail.com>
2021-08-20 14:13:06 +0530

    update pencile arts

--- src/components/pages/Customer_Pencilarts.js
+++ src/components/pages/Customer_Pencilarts.js
@@ -2,35 +2,42 @@ import React from 'react';
 import '../css/Pencilarts.css';
 import RegCusNavbar from '../RegCusNavbar';
 import { Link } from 'react-router-dom';
-export default function Pencilarts (){
+export default function Pencilarts() {
 
-  
+
       return (
-         
-  
 
-        <div className="A"> 
-        <RegCusNavbar/>
-        <center>
-        <h1>De'lart PencilArts</h1></center>
-   
-       
 
-        
-          <div className='C'>
-           <center><img className="Photo" src= { require('../../assests/custom.png').default} alt="" width="1300"></img>
-           </center>
-        
-           <center><Link to = '/customizeart'>
-                       <button className="button">Customize Pencile arts</button>
-                       </Link> </center>
-          
-          </div>
-          
-          
-  
 
-</div>
+            <div className="A">
+                  <RegCusNavbar />
+                  <center>
+                        <h1>De'lart PencilArts</h1></center>
+
+
+
+
+                  <div className='C'>
+                        <center><img className="Photo" src={require('../../assests/custom.png').default} alt="" width="1300"></img>
+                        </center>
+
+                        <center><Link to='/customizeart'>
+                              <button className="button">Customize Pencile arts</button>
+
+
+                        </Link> </center>
+                        <center><Link to='/customizeart'>
+
+
+                              <button className="button">Choose Artist</button>
+                        </Link> </center>
+
+                  </div>
+
+
+
+
+            </div>
       );
-  
+
 }
