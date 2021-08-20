19050cc079295e9f48f1b1d34afcabf86439fc24
DiliniNanayakkara <dlnnimesha@gmail.com>
2021-08-20 20:53:31 +0530

    update pencile arts

--- src/components/pages/pencilartist_profile.js
+++ src/components/pages/pencilartist_profile.js
@@ -13,11 +13,21 @@ export default function Pencilarts() {
             <RegCusNavbar />
 
             <h1>Nimaya Perera</h1>
-            <div className="pencilartistpro">
+            <div className="pencilartistpro1">
                 <img className="pencilartistpro" src="https://www.creativeboom.com/uploads/articles/a4/a40eeac492a143d5bb34412dfe8f275a8834e41c_810.jpg"></img>
             </div>
             <div class="pencilartistname">
+                <h2>Nimaya Perera</h2>
+                <h3>Bio</h3>
+                <p>I am a artist and I do any kind of illustration including digital pencil sketches. I can sketch anything you want me to, from people, to animals, to nature, etc.
 
+
+
+                    I will deliver work according to the package you choose for this gig. You will get a 3000px x 3000px image in 300 dpi, printable resolution.
+
+
+
+                    Please message me first for inquiries before placing an order.</p>
             </div>
 
 
