2e254f0562a24f315ee4485bfc3a5e85688a3eb2
DiliniNanayakkara <dlnnimesha@gmail.com>
2021-08-24 20:40:09 +0530

    update Nav bar styles

---
+++ src/components/pages/data.js
@@ -0,0 +1,18 @@
+export default [
+	{ id: '1', imageName: 'img1.jpg', tag: 'oil' },
+	{ id: '2', imageName: 'img2.jpg', tag: 'pencile' },
+	{ id: '3', imageName: 'img3.jpg', tag: 'oil' },
+	{ id: '4', imageName: 'img4.jpg', tag: 'water' },
+	{ id: '5', imageName: 'img5.jpg', tag: 'water' },
+	{ id: '6', imageName: 'img6.jpg', tag: 'oil' },
+	{ id: '7', imageName: 'img7.jpg', tag: 'pencile' },
+	{ id: '8', imageName: 'img8.jpg', tag: 'pencile' },
+	{ id: '9', imageName: 'img9.jpg', tag: 'water' },
+	{ id: '10', imageName: 'img10.jpg', tag: 'water' },
+	{ id: '11', imageName: 'img11.jpg', tag: 'oil' },
+	{ id: '12', imageName: 'img12.jpg', tag: 'pencile' },
+	{ id: '13', imageName: 'img13.jpg', tag: 'water' },
+	{ id: '14', imageName: 'img14.jpg', tag: 'water' },
+	{ id: '15', imageName: 'img15.jpg', tag: 'oil' },
+	{ id: '16', imageName: 'img16.jpg', tag: 'pencile' }
+];
