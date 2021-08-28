7cc0501a2c0bbda9a5800d40b02a4bf487a4dc32
Amasha Gamage <42163897+amasha98@users.noreply.github.com>
2021-08-28 12:55:08 +0800

    Product Details pages

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -1,39 +1,175 @@
 import React, {useState, useEffect }from 'react';
 import '../css/ArtworkDetail.css';
 import Navbar from '../Navbar';
+import axios from "axios";
 import QuantityPicker from './QuantityPicker';
 import { Link } from 'react-router-dom';
 
-export default function Artworks() {
+
+export default function ProductDetail() {
   const [click, setClick] = useState(false);
-  
+  const [toolList, setToolList] = useState([]);
+  const [toolID, setToolID] = useState(window.location.pathname.split("/")[2]);
   const closeMobileMenu = () => setClick(false);
+
+  useEffect(() => {
+    setToolID(window.location.pathname.split("/")[2]);
+  }, [window.location.pathname]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/productdetail/${toolID}`)
+    .then((response) => {
+           console.log(response.data);
+           setToolList(response.data);
+    });
+  });
+  
   return (
-     <div className="A"> 
+    <div className="A"> 
       <Navbar/>
-      
-      <div className="card">
-      <img className="detailimg" src="https://media.istockphoto.com/photos/paint-brushes-picture-id510006691?s=612x612"></img>
-      <div className="proname">
-          <h2> Art Tool Set </h2>
-          <text>Art tool set with wooden box and <br/>drawing kit with crayons and oil pastels<br/><br/>
-           </text>
-           <text>
-           Rs. 2250<br/></text>
-           <img className="detailimg2" src="https://media.istockphoto.com/photos/paint-brushes-picture-id510006691?s=612x612"></img>
-           <img className="detailimg3" src="https://media.istockphoto.com/photos/paint-brushes-picture-id510006691?s=612x612"></img>
-           <img className="detailimg3" src="https://media.istockphoto.com/photos/paint-brushes-picture-id510006691?s=612x612"></img><br/><br/><br/><br/><br/>
-           
-          <Link
-                              to='/cart'
-                              onClick={closeMobileMenu}
-                          ><button className="cartbtn">Add To Cart        <i class="fa fa-arrow-right" aria-hidden="true"></i></button> 
-                          </Link><br/><br/><br/><br/><br/><br/><br/><hr/>
-      </div>
-      </div>
-      
-     </div>
+      <center>
+      <nav className='nav'>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <li className='item'>
+            <Link
+              to='/products'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+            All 
+            </Link>
+          </li>
+          <li className='item'>
+            <Link
+              to='/productsbrushes'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+            Brushes
+            </Link>
+          </li>
+          <li className='item'>
+            <Link
+              to='/productscanvas'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+            Boards/Canvas
+            </Link>
+          </li>
+          <li className='item'>
+            <Link
+              to='/productssupplies'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+            Painting Supplies
+            </Link>
+          </li>
+          <li className='item'>
+            <Link
+              to='/productseasels'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+            Easels
+            </Link>
+          </li>
+        </ul>
+      </nav>
+  
+      </center>
+     <div>
      
+          {toolList.map((val) => {
+              
+              return <div className="imageanddetail">
+                  <div class="imagedetailrow">
+                  <img className="detailimg" src={'http://localhost:5000/' + val.tool_image}></img>
+                  </div>
+                  <div class="imagedetailrow" >
+                    <h2 class="proname">{val.tool_name}</h2>
+                    
+                    <p class="prodim"> Rs. {val.tool_price}.00</p>
+                    <Link 
+                         to='/cart'
+                         onClick={closeMobileMenu}
+                         >
+                           <button className="cartbtn">Add To cart</button></Link>
+                  </div>
+              </div>
+          // <div className="A"> 
+              
+          //     <div className="carddetail">
+          //     <img className="detailimg" src={'http://localhost:3001/' + val.artwork_image}></img>
+          //     <div className="proname">
+          //         <h2> {val.artwork_name} </h2>
+          //         <text> {val.artwork_description}
+          //         </text>
+          //         <text>60 W * 40 H * 1 D in<br/>Artist : Nimal Perera<br/><br/>Rs. 2250<br/></text>
+                  
+          //         <Link
+          //               to='/cart'
+          //               onClick={closeMobileMenu}
+          //         ><button className="cartbtn">Add To Cart        <i class="fa fa-arrow-right" aria-hidden="true"></i></button> 
+          //                         </Link><br/><br/><br/><br/><br/><br/><br/><hr/>
+          //     </div>
+          //     </div>
+          //   </div>
+      
+       })}
+       </div>
+<br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br>
+<div className="footer">
+        <div className="footercard">
+          <h1>De'lart</h1>
+          <h3>Contact Us</h3>
+          <p>Contact Us</p>
+          <p>Legal Notices</p>
+          <p>General Terms and Conditions</p>
+          <p>My Profile</p>
+          <p>Help Center</p>
+          <p>Call: +119876677</p>
+          <p>Emails:delart@gmail.com</p>
+          
+        </div>
+        <div className="footercard">
+        <h3>Who are we</h3>
+          <p>About Us</p>
+          <p>Our Artists</p>
+          <p>Exibitions</p>
+          <p>Contact</p>
+          <p>Our PRoducts</p>
+         
+         
+        
+        </div>
+        <div className="footercard">
+        <h3>Are you an Artist</h3>
+          <p>How to join Delart</p>
+          <p>Login for Artist</p>
+          <p>Art Gallery</p>
+          <h3>Are you an Buyer</h3>
+          <p>How to join Delart</p>
+          <p>Login for Buyer</p>
+          <p>Art Gallery</p>
+          <p>Artists</p>
+         
+         
+        </div>
+        <div className="footercard">
+        <h3>Sign Up</h3>
+          <p>Artist can usr this system for sell their Artworks.Buyers can purches Artworks.
+            First SignUp ! it's Free!
+          </p>
+          <center><div className="fbutton">SignUp</div></center>
+        </div>
+        </div>
+    </div>
   );
 }
-
