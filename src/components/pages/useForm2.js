0e8abff716056978e124f23a72baa91f53fdb6c5
ThiliniEkanayake <mthiliniekanayake@gmail.com>
2021-08-31 00:50:55 +0530

    Input validation form in buyer registration

--- src/components/pages/useForm2.js
+++ src/components/pages/useForm2.js
@@ -19,6 +19,21 @@ const useForm2 = (callback, validate) => {
       [name]: value,
     });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors(validate(values));
+    setIsSubmitting(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [errors]);
+
+  return { handleChange, handleSubmit, values, errors };
 };
 
 export default useForm2;
