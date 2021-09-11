2e9cd700fe3eb36f511a68b73839ef103ccbee3a
NimayaPerera <54572453+NimayaPerera@users.noreply.github.com>
2021-09-11 13:30:06 +0530

    order part - nimaya

--- src/components/BuyerDashbord/Orders.js
+++ src/components/BuyerDashbord/Orders.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Title from './Title';
-
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
-];
+import React from "react";
+import { useState, useEffect } from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import axios from "axios";
+import Title from "./Title";
+import Swal from "sweetalert2";
 
 function preventDefault(event) {
   event.preventDefault();
@@ -31,8 +21,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Orders() {
+export default function ApprovedOrders() {
   const classes = useStyles();
+
+  const [click, setClick] = useState(false);
+  const [orders, setApprovedOrders] = useState([]);
+
+  const closeMobileMenu = () => setClick(false);
+
+  const pushCart = async (id) => {
+    const res = await axios.post("http://localhost:5000/setCart", {
+      orderId: id,
+      buyerId: localStorage.getItem("buyerId"),
+    });
+    Swal.fire({
+      icon: "success",
+      title: "Item Added !",
+      showConfirmButton: true,
+      timer: 1500,
+    });
+    // window.location.href = `/cart/${id}`;
+  };
+  const approvedOrders = async (id) => {
+    const res = await axios.post("http://localhost:5000/approvedOrders", {
+      buyerId: id,
+    });
+    setApprovedOrders(res.data);
+  };
+
+  useEffect(() => {
+    //optional to pass id
+    const buyerId = 3;
+    localStorage.setItem("buyerId", buyerId);
+    approvedOrders(localStorage.getItem("buyerId"));
+  }, []);
   return (
     <React.Fragment>
       <Title>Recent Orders</Title>
@@ -47,22 +69,26 @@ export default function Orders() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+          {orders.map((order) => (
+            <TableRow key={order.id}>
+              <TableCell>{order.date}</TableCell>
+              <TableCell>{order.name}</TableCell>
+              <TableCell>{order.shipTo}</TableCell>
+              <TableCell>Creadit Card</TableCell>
+              <TableCell align="right">{order.total_price}</TableCell>
+              <TableCell align="right">
+                <button
+                  onClick={() => {
+                    pushCart(order.order_id);
+                  }}
+                >
+                  Add to Cart
+                </button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <div className={classes.seeMore}>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          See more orders
-        </Link>
-      </div>
     </React.Fragment>
   );
 }
---
+++ src/components/pages/orderStatusBuyer.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import "../css/Artworks.css";
+import Navbar from "../Navbar";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+export default function approvedOrdersView() {
+  const [artworkList, setArtworkList] = useState([]);
+  const [click, setClick] = useState(false);
+
+  const closeMobileMenu = () => setClick(false);
+  useEffect(() => {
+    axios.get("http://localhost:5000/approvedOrdersView").then((response) => {
+      setArtworkList(response.data);
+    });
+  }, []);
+    
+  return (
+    <div className="A">
+      <Navbar />
+      <center>
+        <nav className="nav">
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="item">
+              <Link
+                to="/artworks"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                All
+              </Link>
+            </li>
+            <li className="item">
+              <Link
+                to="/artworkshistorical"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Historical
+              </Link>
+            </li>
+            <li className="item">
+              <Link
+                to="/artworksdrawing"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Drawings
+              </Link>
+            </li>
+            <li className="item">
+              <Link
+                to="/artworksfineart"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Fine Arts
+              </Link>
+            </li>
+            <li className="item">
+              <Link
+                to="/artworkspainting"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Paintings
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </center>
+      <div>
+        {artworkList.map((val) => {
+          return (
+            <div className="Arts">
+              <div className="img">
+                {" "}
+                <img src="https://www.creativeboom.com/uploads/articles/a4/a40eeac492a143d5bb34412dfe8f275a8834e41c_810.jpg"></img>
+              </div>
+              <div className="name"> {val.artwork_name}</div>
+              <div className="des">
+                {" "}
+                {val.artwork_category} , {val.artwork_description}
+              </div>
+              <div className="price">Rs. {val.artwork_price}.00</div>
+              <Link to="/artworkdetail" onClick={closeMobileMenu}>
+                <button className="but">
+                  proceed to checkout{" "}
+                  <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                </button>{" "}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      <div className="footer">
+        <div className="footercard">
+          <h1>De'lart</h1>
+          <h3>Contact Us</h3>
+          <p>Contact Us</p>
+          <p>Legal Notices</p>
+          <p>General Terms and Conditions</p>
+          <p>My Profile</p>
+          <p>Help Center</p>
+          <p>Call: +119876677</p>
+          <p>Emails:delart@gmail.com</p>
+        </div>
+        <div className="footercard">
+          <h3>Who are we</h3>
+          <p>About Us</p>
+          <p>Our Artists</p>
+          <p>Exibitions</p>
+          <p>Contact</p>
+          <p>Our PRoducts</p>
+        </div>
+        <div className="footercard">
+          <h3>Are you an Artist</h3>
+          <p>How to join Delart</p>
+          <p>Login for Artist</p>
+          <p>Art Gallery</p>
+          <h3>Are you an Buyer</h3>
+          <p>How to join Delart</p>
+          <p>Login for Buyer</p>
+          <p>Art Gallery</p>
+          <p>Artists</p>
+        </div>
+        <div className="footercard">
+          <h3>Sign Up</h3>
+          <p>
+            Artist can usr this system for sell their Artworks.Buyers can
+            purches Artworks. First SignUp ! it's Free!
+          </p>
+          <center>
+            <div className="fbutton">SignUp</div>
+          </center>
+        </div>
+      </div>
+    </div>
+  );
+}
+
