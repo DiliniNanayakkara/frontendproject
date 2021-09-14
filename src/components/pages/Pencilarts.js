8ba4f09d3694a0545df094f56e44ab0b49190792
DiliniNanayakkara <dlnnimesha@gmail.com>
2021-09-14 18:07:20 +0530

    pencile art page

--- src/components/pages/Pencilarts.js
+++ src/components/pages/Pencilarts.js
@@ -11,19 +11,22 @@ export default function Pencilarts (){
 
         <div className="A"> 
         <Navbar/>
-        <center>
-        <h1>De'lart PencilArts</h1></center>
+       
+              <div className=" commitionbac">
+           <div className="del">         
+           <center><h1>CUSTOM COMMITIONS</h1></center>
+        </div>
+        </div>
+        
+
    
        
 
         
           <div className='C'>
-           <center><img className="Photo" src= { require('../../assests/custom.png').default} alt="" width="1300"></img>
-           </center>
+          
         
-           <center><Link to = '/customizeart'>
-                       <button className="button">Customize Pencile arts</button>
-                       </Link> </center>
+        
           
           </div>
           
