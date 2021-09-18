29ba473df89cc87ecece615244b8a6d538e2af27
NimayaPerera <54572453+NimayaPerera@users.noreply.github.com>
2021-09-18 23:58:25 +0530

    update dashbord info

--- src/components/DashbordInfo.js
+++ src/components/DashbordInfo.js
@@ -1,6 +1,9 @@
 import "./DashbordInfo.css";
+import { useState, useEffect } from "react";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Axios from "axios";
+
 export default function FeaturedInfo() {
   return (
     <div className="featured">
@@ -9,7 +12,8 @@ export default function FeaturedInfo() {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">238</span>
           <span className="featuredMoneyRate">
-            +31 New Users<ArrowUpward className="featuredIcon"/>
+            +31 New Users
+            <ArrowUpward className="featuredIcon" />
           </span>
         </div>
         <span className="featuredSub">3min ago joined last user</span>
@@ -19,7 +23,7 @@ export default function FeaturedInfo() {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">34</span>
           <span className="featuredMoneyRate">
-          -2 <ArrowDownward className="featuredIcon negative"/>
+            -2 <ArrowDownward className="featuredIcon negative" />
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
@@ -29,15 +33,16 @@ export default function FeaturedInfo() {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">Reports</span>
           <span className="featuredMoneyRate">
-          <div>
-          <Link to = '/admin_reports'>
-              {/* <button className="buttonreport">View Reports</button> */}
-          </Link>
-          </div>
-          
+            <div>
+              <Link to="/admin_reports">
+                {/* <button className="buttonreport">View Reports</button> */}
+              </Link>
+            </div>
           </span>
         </div>
-        <span className="featuredSub">Include progress compared to last month</span>
+        <span className="featuredSub">
+          Include progress compared to last month
+        </span>
       </div>
     </div>
   );
