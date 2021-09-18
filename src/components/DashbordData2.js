aa176f1a00511bf130abff29e60d8fa1ed0e5055
NimayaPerera <54572453+NimayaPerera@users.noreply.github.com>
2021-09-18 23:56:01 +0530

    update js

--- src/components/DashbordData2.js
+++ src/components/DashbordData2.js
@@ -14,6 +14,41 @@ export default function WidgetLg() {
 
   const [employeeList, setEmployeeList] = useState([]);
 
+  useEffect(() => {
+    Axios.get("http://localhost:5000/Allartist").then((response) => {
+      setEmployeeList(response.data);
+    });
+  });
+  return(
+    <div>
+    
+ 
+
+    <div className="widgetLg">
+      <center><p className="titlem">Artist Details</p></center>
+<center>
+  
+        <table className="Artist">
+          <thead>
+        <tr>
+    <th>Email</th>
+    <th>First_name</th>
+    <th>Last_name</th>
+    <th>Contact_no</th>
+    <th>User_role</th>
+    <th>NIC</th>
+  </tr>  </thead>
+  {employeeList.map((val, key) => {
+    return (
+  <tbody>
+  <tr>
+    <td>{val.email}</td>
+    <td>{val.first_name}</td>
+    <td>{val.last_name}</td>
+    <td>{val.contact_no}</td>
+    <td>{val.user_role}</td>
+    <td>{val.nic}</t
+
           <div className="widgetSmUser">
             <span className="widgetSmUsername">Cristo Nish</span>
             <span className="widgetSmUserTitle">Artist</span>
