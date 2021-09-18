d3dca10a9cb48e7b91aed054dc3ab0b0c842f5fb
NimayaPerera <54572453+NimayaPerera@users.noreply.github.com>
2021-09-18 23:41:01 +0530

    correct dashborddata

--- src/components/DashbordData.js
+++ src/components/DashbordData.js
@@ -2,8 +2,8 @@ import "./DashbordData.css";
 
 import { useState, useEffect } from "react";
 import Axios from "axios";
-export default function WidgetLg() {
 
+export default function WidgetLg() {
   const [email, setemail] = useState("");
   const [first_name, setfirst] = useState("");
   const [last_name, setlast] = useState("");
@@ -15,52 +15,46 @@ export default function WidgetLg() {
     return <button className={"widgetLgButton " + type}>{type}</button>;
   };
   useEffect(() => {
-    Axios.get("http://local
-          <td className="widgetLgUser">
-            <img
-              src="https://ssc98hsc00.com/uploads/user/340558.jpeg"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Mark Mexdo</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">27</td>
-          <td className="widgetLgStatus">
-            <Button type="Pending" />
-          </td>
-        </tr>
-     
-    <div className="widgetLg">
-      <center><p className="titlem">Saff Details</p></center>
-<center>
-  
-        <table className="Artist">
-          <thead>
-        <tr>
-    <th>Email</th>
-    <th>First_name</th>
-    <th>Last_name</th>
-    <th>Contact_no</th>
-    <th>User_role</th>
-    <th>NIC</th>
-  </tr>  </thead>
-  {employeeList.map((val, key) => {
-    return (
-  <tbody>
-  <tr>
-    <td>{val.email}</td>
-    <td>{val.first_name}</td>
-    <td>{val.last_name}</td>
-    <td>{val.contact_no}</td>
-    <td>{val.user_role}</td>
-    <td>{val.nic}</td>
-  </tr>
- 
-  </tbody>
-    );
-  })}
+    Axios.get("http://localhost:5000/staff").then((response) => {
+      setEmployeeList(response.data);
+    });
+  });
 
-        </table>
-
+  return (
+    <div>
+      <div className="widgetLg">
+        <center>
+          <p className="titlem">Saff Details</p>
         </center>
+        <center>
+          <table className="Artist">
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>First_name</th>
+                <th>Last_name</th>
+                <th>Contact_no</th>
+                <th>User_role</th>
+                <th>NIC</th>
+              </tr>{" "}
+            </thead>
+            {employeeList.map((val, key) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td>{val.email}</td>
+                    <td>{val.first_name}</td>
+                    <td>{val.last_name}</td>
+                    <td>{val.contact_no}</td>
+                    <td>{val.user_role}</td>
+                    <td>{val.nic}</td>
+                  </tr>
+                </tbody>
+              );
+            })}
+          </table>
+        </center>
+      </div>
+    </div>
+  );
+}
