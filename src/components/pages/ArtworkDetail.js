106016a13f011e821d574f712ccb76a62c3e5985
Amasha Gamage <42163897+amasha98@users.noreply.github.com>
2021-09-19 12:13:50 +0800

    Update ArtworkDetail.js

--- src/components/pages/ArtworkDetail.js
+++ src/components/pages/ArtworkDetail.js
@@ -9,6 +9,7 @@ import { Link } from 'react-router-dom';
 export default function ArtworkDetail() {
   const [click, setClick] = useState(false);
   const [artworkList, setArtworkList] = useState([]);
+  const [location, setLocation] = useState("");
   const [artworkID, setArtworkID] = useState(window.location.pathname.split("/")[2]);
   const closeMobileMenu = () => setClick(false);
 
@@ -23,6 +24,20 @@ export default function ArtworkDetail() {
            setArtworkList(response.data);
     });
   });
+  
+  const sendRequest = () => {
+    axios
+      .post("http://localhost:5000/request", {
+        artworkName: artworkList.artwork_name,
+        location: location,
+        artist: artworkList.artist_email,
+        artworkPrice: artworkList.artwork_price,
+        
+      })
+      .then(() => {
+        console.log("success");
+      });
+    }
   
   return (
     <div className="A"> 
@@ -90,11 +105,89 @@ export default function ArtworkDetail() {
                   <div class="imagedetailrow" >
                     <h2 class="proname">{val.artwork_name}</h2>
                     <p class="prodes">{val.artwork_description}</p>
-                    <p class="prodim">Artwork Dimensions &nbsp; &nbsp;:&nbsp; &nbsp; </p><br></br>
+                    <p class="prodim">Artwork Dimensions (Width*Height)&nbsp; &nbsp;:&nbsp; &nbsp; {val.artwork_dimension} cm</p><br></br>
                     <p class="prodim"> Artist &nbsp; &nbsp;  :   &nbsp; &nbsp; {val.artwork_artist}</p><br></br>
                     <p class="prodim"> Rs. {val.artwork_price}.00</p>
+                    <p class="prodim"> Delivery location (District) : 
+                    <select
+                       className="district"
+                       onChange={(event) => {
+                        setLocation(event.target.value);
+                       }}
+                        name="location"
+                       
+                      >
+                        <option value="">- Choose your delivery district -</option>
+                        <option value="Ampara">Ampara</option>
+                        <option value="Anuradhapura">Anuradhapura</option>
+                        <option value="Badulla">Badulla</option>
+                        <option value="Batticaloa">Batticaloa</option>
+                        <option value="Colombo">Colombo</option>
+                        <option value="Galle">Galle</option>
+                        <option value="Gampaha">Gampaha</option>
+                        <option value="Hambantota">Hambanthota</option>
+                        <option value="Jaffna">Jaffna</option>
+                        <option value="Kalutara">Kalutara</option>
+                        <option value="Kandy">kandy</option>
+                        <option value="Kegalle">kegalle</option>
+                        <option value="Kilinochchi">Kilinochchi</option>
+                        <option value="Kurunegala">Kurunegala</option>
+                        <option value="Mannar">Mannar</option>
+                        <option value="Matale">Matale</option>
+                        <option value="Matara">Matara</option>
+                        <option value="Moneragala">Moneragala</option>
+                        <option value="Mullaitivu">Mullaitivu</option>
+                        <option value="NuwaraEliya">Nuwara Eliya</option>
+                        <option value="Polonnaruwa">Polonnaruwa</option>
+                        <option value="Puttalam">Puttalam</option>
+                        <option value="Rathnapura">Ratnapura</option>
+                        <option value="Trincomalee">Trincomalee</option>
+                        <option value="Vavuniya">Vavuniya</option>
+                        {/* <option value="Western">Western</option>
+                        <option value="Central">Central</option>
+                        <option value="Southern">Southern</option>
+                        <option value="Uva">Uva</option>
+                        <option value="Sabaragamuwa">Sabaragamuwa</option>
+                        <option value="North Western">North Western</option>
+                        <option value="North Central">North Central</option>
+                        <option value="Nothern">Nothern</option>
+                        <option value="Eastern">Eastern</option> */}
+                        {/* <option value="Thailand">Ampara</option>
+                        <option value="Japan">Anuradhapura</option>
+                        <option value="Brazil">Badulla</option>
+                        <option value="Brazil">Batticaloa</option>
+                        <option value="Brazil">Colombo</option>
+                        <option value="Brazil">Galle</option>
+                        <option value="Brazil">Gampaha</option>
+                        <option value="Brazil">Hambanthota</option>
+                        <option value="Brazil">Jaffna</option>
+                        <option value="Brazil">Kalutara</option>
+                        <option value="Brazil">kandy</option>
+                        <option value="Brazil">kegalle</option>
+                        <option value="Brazil">Kilinochchi</option>
+                        <option value="Brazil">Kurunegala</option>
+                        <option value="Brazil">Mannar</option>
+                        <option value="Brazil">Matale</option>
+                        <option value="Brazil">Matara</option>
+                        <option value="Brazil">Moneragala</option>
+                        <option value="Brazil">Mullaitivu</option>
+                        <option value="Brazil">Nuwara Eliya</option>
+                        <option value="Brazil">Polonnaruwa</option>
+                        <option value="Brazil">Puttalam</option>
+                        <option value="Brazil">Ratnapura</option>
+                        <option value="Brazil">Trincomalee</option>
+                        <option value="Brazil">Vavuniya</option> */}
+                      </select>
+            {/* <input
+              type="text"
+              name="location"
+              onChange={(event) => {
+                setLocation(event.target.value);
+              }}
+            />  */}
+            </p><br></br>
                     <Link 
-                         to='/cart'
+                         to={`/artworkrequest/${val.artwork_id}/${location}`} 
                          onClick={closeMobileMenu}
                          >
                            <button className="cartbtn">Add To cart</button></Link>
@@ -113,7 +206,7 @@ export default function ArtworkDetail() {
           //         <Link
           //               to='/cart'
           //               onClick={closeMobileMenu}
-          //         ><button className="cartbtn">Add To Cart        <i class="fa fa-arrow-right" aria-hidden="true"></i></button> 
+          //         ><button > Continue       <i class="fa fa-arrow-right" aria-hidden="true"></i></button> 
           //                         </Link><br/><br/><br/><br/><br/><br/><br/><hr/>
           //     </div>
           //     </div>
