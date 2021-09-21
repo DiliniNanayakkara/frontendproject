b5bfbf6703407f3c0cc2d268aab49b4b70aded6a
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-22 04:21:35 +0530

    create css for FAQ

--- src/components/pages/moderator/FnQ.js
+++ src/components/pages/moderator/FnQ.js
@@ -1,60 +1,60 @@
-import React from 'react';
+import React from "react";
+import "../../css/moderator/FaQ.css";
 
 export default function FnQ() {
-    return (
-      <div>
-        {/* <ModeratorNavbar />
+  return (
+    <div>
+      {/* <nav className="topnav">Hello world</nav> */}
+      {/* <ModeratorNavbar />
         <HelpCenterHomeInfo /> */}
-        <nav>Hello world</nav>
-        <><h3>What are the benefits of being part of the DelArt?</h3>
-        <h3>What is the difference between exhibitin and art gallery?
-        </h3>
+{/* //************1st DIV tag********** */ }
+<span>
+        <div className="div1">
+        <h3>What are the benefits of being part of the DelArt?</h3>
+        <h3>What is the difference between exhibitin and art gallery?</h3>
         <h3>Is my personal information safe?</h3>
 
+        </div>
 
-        <p1>Your privacy is of utmost importance to us. 
-         You can read our Privacy Policy here, as it is part of our Terms of Service, for further information</p1>
+        {/* *************2nd DIV tag************ */}
 
+        <div>
+
+        <p1>
+          Your privacy is of utmost importance to us. You can read our Privacy
+          Policy here, as it is part of our Terms of Service, for further
+          information
+        </p1>
 
         <h3>Do I pay my seller directly?</h3>
 
-       <p1>All payments are made through the Fiverr site via the Order page, 
-        and all buyers pay Fiverr for orders in advance. 
-        This is for your own safety as  Fiverr will not be able to assist you if you pay your seller outside of the platform. 
-        In the case where you are been asked to use an alternative payment method, 
-        please report it to Customer Service immediately.
- </p1>
 
-<h3>How do I buy Artwor using the DelArt?</h3>
+        </div>
 
-<h3>How do I cancel my order?</h3>
-</>
+        {/* ****** 3rd DIV tag********** */}
 
-      </div>
-    );
-  }
-  
+        <div className="div3">
+        <p1>
+          All payments are made through the Fiverr site via the Order page, and
+          all buyers pay Fiverr for orders in advance. This is for your own
+          safety as Fiverr will not be able to assist you if you pay your seller
+          outside of the platform. In the case where you are been asked to use
+          an alternative payment method, please report it to Customer Service
+          immediately.
+        </p1>
 
-<><h3>What are the benefits of being part of the DelArt?</h3>
-<h3>What is the difference between exhibitin and art gallery?
-</h3>
-<h3>Is my personal information safe?</h3>
+        <h3>How do I buy Artwor using the DelArt?</h3>
 
+        <h3>How do I cancel my order?</h3>
 
-<p1>Your privacy is of utmost importance to us. 
-    You can read our Privacy Policy here, as it is part of our Terms of Service, for further information</p1>
+        </div>
 
+      
+        
+        </span> 
+      
+     
+    </div>
+  );
+}
 
-    <h3>Do I pay my seller directly?</h3>
-
-    <p1>All payments are made through the Fiverr site via the Order page, 
-        and all buyers pay Fiverr for orders in advance. 
-        This is for your own safety as  Fiverr will not be able to assist you if you pay your seller outside of the platform. 
-        In the case where you are been asked to use an alternative payment method, 
-        please report it to Customer Service immediately.
-</p1>
-
-<h3>How do I buy Artwor using the DelArt?</h3>
-
-<h3>How do I cancel my order?</h3>
-</>
