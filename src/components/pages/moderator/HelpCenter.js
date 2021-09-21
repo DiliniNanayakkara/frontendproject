ac4fcede3ad5ee78674ba9168c8bdccb4b070343
NimayaPerera <54572453+NimayaPerera@users.noreply.github.com>
2021-09-21 22:41:37 +0530

    help center

--- src/components/pages/moderator/HelpCenter.js
+++ src/components/pages/moderator/HelpCenter.js
@@ -1,27 +1,13 @@
-import React from 'react';
-import SlideMenu from './SlideMenu';
-import ModeratorNavbar from './ModeratorNavbar';
-import HelpCenterHomeInfo from './HelpCenterHomeInfo';
+import React from "react";
+import SlideMenu from "./SlideMenu";
+import ModeratorNavbar from "./ModeratorNavbar";
+import HelpCenterHomeInfo from "./HelpCenterHomeInfo";
 
-
-
-
-export default function Dashbord (){
-
-  
-      return (
-            <div>
-                   <ModeratorNavbar />
-                   <HelpCenterHomeInfo />
-                  
-                   
-         
-
-            </div>
-           
-
-  
-      );
-      
-  
+export default function Dashbord() {
+  return (
+    <div>
+      <ModeratorNavbar />
+      <HelpCenterHomeInfo />
+    </div>
+  );
 }
