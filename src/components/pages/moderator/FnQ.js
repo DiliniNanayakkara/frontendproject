7318ec2e48e44a2ab598bb051a68454191a9b417
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-22 01:25:22 +0530

    add question 3

--- src/components/pages/moderator/FnQ.js
+++ src/components/pages/moderator/FnQ.js
@@ -4,4 +4,6 @@ import React from 'react';
 
 <><h3>What are the benefits of being part of the DelArt?</h3>
 <h3>What is the difference between exhibitin and art gallery?
-</h3></>
+</h3>
+<h3>Is my personal information safe?</h3>
+</>
