ba277febf77c133b7752bc17fd05292eb585d316
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-22 01:23:43 +0530

    add question 2

--- src/components/pages/moderator/FnQ.js
+++ src/components/pages/moderator/FnQ.js
@@ -2,4 +2,6 @@ import React from 'react';
 
 
 
-<h3>What are the benefits of being part of the DelArt?</h3>
+<><h3>What are the benefits of being part of the DelArt?</h3>
+<h3>What is the difference between exhibitin and art gallery?
+</h3></>
