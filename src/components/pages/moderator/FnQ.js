b9ea33613c16235f675cf480b17659bb93deb392
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-22 01:26:34 +0530

    add Answer for question 3

--- src/components/pages/moderator/FnQ.js
+++ src/components/pages/moderator/FnQ.js
@@ -6,4 +6,8 @@ import React from 'react';
 <h3>What is the difference between exhibitin and art gallery?
 </h3>
 <h3>Is my personal information safe?</h3>
+
+
+<p1>Your privacy is of utmost importance to us. 
+    You can read our Privacy Policy here, as it is part of our Terms of Service, for further information</p1>
 </>
