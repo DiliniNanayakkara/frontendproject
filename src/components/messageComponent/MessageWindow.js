0c0b5b78e4873199960d910f0346f8b0ce50963d
ThiliniEkanayake <mthiliniekanayake@gmail.com>
2021-09-22 12:59:26 +0530

    Updated message template

---
+++ src/components/messageComponent/MessageWindow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { ChatEngine } from "react-chat-engine";
+function App() {
+  return (
+    <ChatEngine
+      projectID="b5b053fd-e975-453e-a1ab-5ba0bd76e6cb"
+      userName="dilini"
+      userSecret="asd123"
+    />
+  );
+}
+
+export default App;
