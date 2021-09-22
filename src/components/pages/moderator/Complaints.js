317b4b0e78ba85656e1983a49483715de2f42472
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-22 06:06:35 +0530

    import moderator navbar for tools page

---
+++ src/components/pages/moderator/Complaints.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../../css/moderator/FaQ.css";
+import ModeratorNavbar from "./ModeratorNavbar";
+
+export default function FnQ() {
+  return (
+    <div >
+      {/* <nav className="topnav">Hello world</nav> */}
+     <ModeratorNavbar />
+   
+
+      
+        
+     
+      
+     
+    </div>
+  );
+}
+
