07e8d634c16c50f2efa87b60674a218a2c576d51
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-22 05:37:46 +0530

    add css into questions

--- src/components/pages/moderator/FnQ.js
+++ src/components/pages/moderator/FnQ.js
@@ -27,9 +27,9 @@ export default function FnQ() {
             </div>
         <h3 className="question"> What are the benefits of being part of the DelArt?</h3>
 
-        <h3>What is the difference between exhibitin and art gallery?</h3>
+        <h3 className="question">What is the difference between exhibitin and art gallery?</h3>
 
-        <h3>Is my personal information safe?</h3>
+        <h3 className="question">Is my personal information safe?</h3>
 
         <p1>
           Your privacy is of utmost importance to us. You can read our Privacy
@@ -37,7 +37,7 @@ export default function FnQ() {
           information
         </p1>
 
-        <h3>Do I pay my seller directly?</h3>
+        <h3 className="question">Do I pay my seller directly?</h3>
         <p1>
           All payments are made through the Fiverr site via the Order page, and
           all buyers pay Fiverr for orders in advance. This is for your own
@@ -46,9 +46,11 @@ export default function FnQ() {
           an alternative payment method, please report it to Customer Service
           immediately.
         </p1>
-        <h3>How do I buy Artwor using the DelArt?</h3>
 
-        <h3>How do I cancel my order?</h3>
+
+        <h3 className="question">How do I buy Artwor using the DelArt?</h3>
+
+        <h3 className="question">How do I cancel my order?</h3>
 
         <p1>
           All payments are made through the Fiverr site via the Order page, and
