f503a4a13d0a2e5b1f173f1496bbd2d4831737ab
ThiliniEkanayake <mthiliniekanayake@gmail.com>
2021-09-22 18:42:24 +0530

    Updated Create Exhibition Form

--- src/components/pages/Createexhibition.js
+++ src/components/pages/Createexhibition.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
-import '../../App.css';
-
+import "../../App.css";
 
 // export default function Login() {
 //   return (
@@ -24,52 +23,46 @@ import '../../App.css';
 //   );
 // }
 
-
-
 const useStyles = makeStyles((theme) => ({
-    root: {
-      height: '100vh',
-    },
-    image: {
-      backgroundImage: 'url(https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/Web-design-vs-web-de.jpg?auto=format&q=60&w=1860&h=1860&fit=crop&crop=faces)',
-    
-    
-      
-      backgroundRepeat: 'no-repeat',
-      backgroundColor:
-        theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    },
-    paper: {
-      margin: theme.spacing(8, 4),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage:
+      "url(https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/Web-design-vs-web-de.jpg?auto=format&q=60&w=1860&h=1860&fit=crop&crop=faces)",
 
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Customized_Artist() {
-    const classes = useStyles();
-  
-    return (
+  const classes = useStyles();
 
-      <>
-       
-
-       
+  return (
+    <>
       <Grid container component="main" className={classes.root}>
         <CssBaseline />
         <Grid item xs={false} sm={4} md={7} className={classes.image} />
@@ -82,7 +75,7 @@ export default function Customized_Artist() {
               Create Exhibition
             </Typography>
             <form className={classes.form} noValidate>
-            <h3>ArtistId:</h3>
+              <h3>ArtistId:</h3>
               <TextField
                 variant="outlined"
                 margin="normal"
@@ -106,19 +99,24 @@ export default function Customized_Artist() {
                 id="Category type"
                 // autoComplete="current-password"
               />
-               <h3>Add Images:</h3>
+              <h3>Upload Artworks:</h3>
+
               <TextField
                 variant="outlined"
                 margin="normal"
                 required
                 fullWidth
-                name="Add Images"
-                label="Add Images"
-                type="Add Images"
+                name="images"
+                type="file"
+                accept="image/*"
+                multiple={false}
+                //onChange={imageHandler}
+                //label="Add Images"
+                //type="Add Images"
                 id="Add Images"
                 // autoComplete="current-password"
               />
-              <button>choose files</button>
+
               <h3>Publish Date: </h3>
               <TextField
                 variant="outlined"
@@ -157,19 +155,13 @@ export default function Customized_Artist() {
                 Create Exhibitions
               </Button>
               <Grid container>
-               
-                 <Grid item>
-                 
-                </Grid> 
+                <Grid item></Grid>
               </Grid>
-              <Box mt={5}>
-                
-              </Box>
+              <Box mt={5}></Box>
             </form>
           </div>
         </Grid>
-       </Grid>
-
-       </>
-    );
-  }
+      </Grid>
+    </>
+  );
+}
